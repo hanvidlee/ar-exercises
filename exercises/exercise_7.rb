5ecb7bac8702ba03ee9213800fb393a4c4bf5ba7
hanvidlee <hanvidlee@gmail.com>
2023-05-02 03:21:46 -0400

    complete exercises, and validate columns

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,17 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter a store name"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Successfully created store #{store_name}"
+else 
+  puts "there was an error in creating the store"
+  new_store.errors.full_messages.each do |message|
+    puts message
+  end
+end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,15 @@
 class Store < ActiveRecord::Base
+  has_many :employees
 
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :carries_mens_or_womens_apparel
+  
+
+  def carries_mens_or_womens_apparel
+    if !mens_apparel && !womens_apparel
+      errors.add(:mens_apparel, "mens or womens apparel must be true")
+      errors.add(:womens_apparel, "mens or womens apparel must be true")
+    end
+  end
 end
